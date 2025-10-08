0fd82d65f417144e9331ec3845d6393f6f624df8
kekeil <simporesalathia@gmail.com>
2025-10-08 14:05:11 +0000

    fix admin et (admin)

--- app/init-admin/page.tsx
+++ app/init-admin/page.tsx
@@ -1,4 +1,4 @@
-import InitAdmin from '@/components/admin/InitAdmin';
+import InitAdmin from '@/components/adminInit/InitAdmin';
 
 export default function InitAdminPage() {
   return <InitAdmin />;
